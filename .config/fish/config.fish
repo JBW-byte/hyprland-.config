baad99455b19a0be5b85d4f8d8e7e8e98c9a6c91
L.E.D. <jamesyorkshire75@gmail.com>
2025-05-13 19:41:03 +0100

    Add files via upload

---
+++ .config/fish/config.fish
@@ -0,0 +1,9 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
+
+oh-my-posh init fish --config /home/james/.config/posh/gruvbox.omp.json | source
